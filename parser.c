3af37a036ab56ac2903ac2697da484e2a97727af
Blagoje Blagojevic <blagojeblagojevic562@gmail.com>
2024-11-10 15:18:09 +0100

    Update parser.c

--- parser.c
+++ parser.c
@@ -1,34 +1,45 @@
 
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
 #include<string.h>
 #include "lex.h"
-//#include "lex.c"
+//#define LOG_STACK
+//#include "bvm.h"
+
 
 //void Parser(Token *tokens);
 
 
-#define WRITE_TO_FILE(str)\
-		{char buffer[100]; \
-		memset(buffer, '\0', sizeof(char)*100);\
-		sprintf(buffer, str, tokens[counterTokens].value);\
-		fwrite(buffer,sizeof(char), strlen(buffer), f); counterInstruction++;\
-		}
-
 
 #define WORD_COMPARE(KEYWORD) (!strcmp(keywords[KEYWORD], tokens[counterTokens].value))
 
+int ValueToNum(char *str) {
+	long int num;
+	char *end;
+	num = strtol(str, &end, 10);
+	//printf("Num is %d", (int)num);
+	return (int)num;
+	}
 
-void Parser(Token *tokens, const char* name) {
+
+void Parser(Token *tokens, Bvm *bvm) {
+
+	//system("pause");
+
 	int counterTokens = 0, counterInstruction = 0;
-	FILE *f = fopen(name, "w");
-	if(f == NULL) {
-		ERROR("We have not init a file why who now\n");
-		}
+
+	Stack ifStack;
+	initStack(&ifStack);
+	int stackSize = 0;
+	//
 	while(tokens[counterTokens].type != TYPE_EOF) {
-		printf("Num of token %d\n", counterTokens);
+
+		//printf("Num of token %d\n", counterTokens);
 		//system("pause");
+
+		//memset()
 		if(counterTokens >= MAX_TOKENS) {
 			ERROR("Error in max num of tokens\n");
 			}
@@ -37,37 +48,62 @@ void Parser(Token *tokens, const char* name) {
 		switch(tokens[counterTokens].type) {
 			case TYPE_CONST: {
 					//char buffer[100];
-					WRITE_TO_FILE("PUSH %s\n");
+					int valueNum = ValueToNum(tokens[counterTokens].value);
+					bvm->instruction[counterInstruction].type = PUSH;
+					bvm->instruction[counterInstruction].operand._asI64 = valueNum;
+					printf("\nconst %d, push\n", valueNum);
 					counterTokens++;
+					counterInstruction++;
+					stackSize++;
 					break;
 					}
 			//MATH
 			case TYPE_OPERATION: {
 					switch(tokens[counterTokens].value[0]) {
 						case '+': {
-								//if add type checking heare change
-								WRITE_TO_FILE("ADD 0\n");
+								int valueNum = ValueToNum(tokens[counterTokens].value);
+								bvm->instruction[counterInstruction].type = ADD;
+								bvm->instruction[counterInstruction].operand._asI64 = valueNum;
+								printf("\noperation %s, add\n", tokens[counterTokens].value);
 								counterTokens++;
+								counterInstruction++;
 
 								break;
 								}
 						case '-': {
-								WRITE_TO_FILE("DEC 0\n");
+
+								int valueNum = ValueToNum(tokens[counterTokens].value);
+								bvm->instruction[counterInstruction].type = DEC;
+								bvm->instruction[counterInstruction].operand._asI64 = valueNum;
+								printf("\noperation %s, dec\n", tokens[counterTokens].value);
 								counterTokens++;
+								counterInstruction++;
 
 								break;
 								}
 						case '*': {
-								WRITE_TO_FILE("MUL 0\n");
+
+								int valueNum = ValueToNum(tokens[counterTokens].value);
+								bvm->instruction[counterInstruction].type = MUL;
+								bvm->instruction[counterInstruction].operand._asI64 = valueNum;
+								printf("\noperation %s, mul\n", tokens[counterTokens].value);
 								counterTokens++;
+								counterInstruction++;
+
 								break;
 								}
 						case '/': {
-								WRITE_TO_FILE("DIV 0\n");
+
+								int valueNum = ValueToNum(tokens[counterTokens].value);
+								bvm->instruction[counterInstruction].type = DIV;
+								bvm->instruction[counterInstruction].operand._asI64 = valueNum;
+								printf("\noperation %s, div\n", tokens[counterTokens].value);
 								counterTokens++;
+								counterInstruction++;
+
 								break;
 								}
-
+							stackSize--;
 						default: {
 								ERROR("Not implemented math operation or somthing\n");
 								break;
@@ -79,8 +115,66 @@ void Parser(Token *tokens, const char* name) {
 					//search thru all keywords somw fensi prob maybe hash
 					//PRINT FOR NOW ONLY INTEGERS
 					if(WORD_COMPARE(KEYWORD_PRINT)) {
-						WRITE_TO_FILE("PRINT 0\n");
+						//int valueNum = ValueToNum(tokens[counterTokens].value); HANDLING DEPEND ON TYPE OF A FILE
+						bvm->instruction[counterInstruction].type = PRINT;
+						bvm->instruction[counterInstruction].operand._asI64 = 0;
+						printf("\n%s, print\n", tokens[counterTokens].value);
 						counterTokens++;
+						counterInstruction++;
+
+						}
+					else if (WORD_COMPARE(KEYWORD_DUP)) {
+						bvm->instruction[counterInstruction].type = DUP;
+						bvm->instruction[counterInstruction].operand._asI64 = 0;
+						printf("\n%s, dup\n", tokens[counterTokens].value);
+						counterTokens++;
+						counterInstruction++;
+						stackSize++;
+						}
+
+					else if(WORD_COMPARE(KEYWORD_IF)) {
+						stackSize-=2;
+						stackPush(&ifStack, stackSize);
+						//stackSize = 0;
+						int endTokenPos = counterInstruction, numIF = 0;
+						while(tokens[endTokenPos].type != TYPE_EOF) {
+							if(!strcmp(tokens[endTokenPos].value, keywords[KEYWORD_IF])) {
+								numIF++;
+								}
+							if(!strcmp(tokens[endTokenPos].value, keywords[KEYWORD_END])) {
+								numIF--;
+								if(numIF == 0) {
+									break;
+									}
+								}
+
+							endTokenPos++;
+							}
+
+						//system("pause");
+						if(tokens[endTokenPos].type == TYPE_EOF) {
+							ERROR_BREAK("End token not provided\n");
+							}
+						bvm->instruction[counterInstruction].type = JMPF;
+						bvm->instruction[counterInstruction].operand._asI64 = (i64)(endTokenPos);
+						printf("\n%d, JUMPF\n", endTokenPos);
+						counterTokens++;
+						counterInstruction++;
+
+						}
+					else if(WORD_COMPARE(KEYWORD_ELSE)) {
+						bvm->instruction[counterInstruction].type = SETSP;
+						bvm->instruction[counterInstruction].operand._asI64 = stackPop(&ifStack)._asI64;
+						stackSize = bvm->instruction[counterInstruction].operand._asI64;
+						printf("\nreturn sp = %d, else\n", bvm->instruction[counterInstruction].operand._asI64);
+						counterTokens++;
+						counterInstruction++;
+						}
+					else if(WORD_COMPARE(KEYWORD_END)) {
+						bvm->instruction[counterInstruction].type = NOP;
+						bvm->instruction[counterInstruction].operand._asI64 = 0;
+						counterTokens++;
+						counterInstruction++;
 						}
 					else {
 						//counterInstruction++;
@@ -89,52 +183,30 @@ void Parser(Token *tokens, const char* name) {
 					break;
 					}
 			case TYPE_LOGICOPERATOR: {
-					//GET A FIRST END TOKEN
-					int endTokenPosition = counterInstruction, numIf = 0, counterTemp = counterTokens;
-					int numOfIf = 1;
-					
-					while(tokens[counterTemp].type != TYPE_EOF){
-						printf("CI %d ETP %d NIF %d\n", counterInstruction, endTokenPosition, numOfIf);
-						//system("pause");
-						if(!strcmp(tokens[counterTemp].value, keywords[KEYWORD_IF]) 
-							&& tokens[counterTemp].type == TYPE_KEYWORD){
-								numOfIf++;
-								endTokenPosition++;
-							}
-						
-						else if(!strcmp(tokens[counterTemp].value, keywords[KEYWORD_END]) 
-							&& tokens[counterTemp].type == TYPE_KEYWORD){
-								//endTokenPosition++;
-								numOfIf--;
-								if(numOfIf == 0){
-									endTokenPosition+=2;
-									break;
-								}
-								
-							}
-						else{
-							endTokenPosition++;
-						}
-						
-						counterTemp++;
-					}	
-					
-					if(tokens[counterTemp].type == TYPE_EOF) {
-						ERROR("End Token not provided Token: %s\n", tokens[counterTokens].value);
-						}
-
-					if(strlen(tokens[counterTokens].value) == 1) {
-						WRITE_TO_FILE("IF %s\n");
-						char buffer[100];
-						sprintf(buffer, "JMPF %d\n", endTokenPosition - 1);
-						fwrite(buffer, sizeof(char), strlen(buffer), f);
-						counterInstruction++;
+					//IF EQUAL
+					if(!strcmp(tokens[counterTokens].value, logicOperators[LOGIC_E])) {
+						bvm->instruction[counterInstruction].type = IF;
+						bvm->instruction[counterInstruction].operand._asI64 = 2;
+						printf("\nlogicoperator %s, IF\n", tokens[counterTokens].value);
 						counterTokens++;
-
+						counterInstruction++;
 						}
-					else {
-						//Todo implement other shit AKA >=, <=
+					else if (!strcmp(tokens[counterTokens].value, logicOperators[LOGIC_G])) {
+						bvm->instruction[counterInstruction].type = IF;
+						bvm->instruction[counterInstruction].operand._asI64 = 0;
+						printf("\nlogicoperator %s, IF\n", tokens[counterTokens].value);
+						counterTokens++;
+						counterInstruction++;
 						}
+					else if(!strcmp(tokens[counterTokens].value, logicOperators[LOGIC_L])) {
+						bvm->instruction[counterInstruction].type = IF;
+						bvm->instruction[counterInstruction].operand._asI64 = 1;
+						printf("\nlogicoperator %s, IF\n", tokens[counterTokens].value);
+						counterTokens++;
+						counterInstruction++;
+						}
+					//TD add all other logic operators
+					else {};
 					break;
 					}
 			default: {
@@ -144,9 +216,12 @@ void Parser(Token *tokens, const char* name) {
 					}
 			}
 
-
+		//system("pause");
 		}
-	WRITE_TO_FILE("END 0");
-	fclose(f);
+
+	bvm->instruction[counterInstruction].type = END;
+	bvm->instruction[counterInstruction].operand._asU64 = 0;
+	bvm->numOfInstructions = (u64)counterTokens;
+
 
 	}
