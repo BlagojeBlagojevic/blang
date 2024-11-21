11e8ef2bdf6476a93407ebd4d822b5e864919fa9
Blagoje Blagojevic <blagojeblagojevic562@gmail.com>
2024-11-21 20:48:28 +0100

    Create parser.h

---
+++ parser.h
@@ -0,0 +1,22 @@
+#ifndef PARSER_H
+#define PARSER_H
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<string.h>
+#include "lex.h"
+#define LOG_STACK
+#define BVM_IMPLEMENTATION
+#include "bvm.h"
+#define MAX_VARS 500
+#define MAX_VARS_NAME 100
+
+typedef struct {
+	int adress[MAX_VARS];
+	char* name[MAX_VARS];
+	int sp;
+
+	} VarStack;
+	
+void Parser(Token *tokens, Bvm *bvm);
+#endif
