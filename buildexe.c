efa8a5aa26878bc9df7db5dc42d9f915d09c952c
Blagoje Blagojevic <blagojeblagojevic562@gmail.com>
2025-03-03 12:19:43 +0100

    Add files via upload

---
+++ buildexe.c
@@ -0,0 +1,13 @@
+//THIS FILE IS FOR BUILDING executable FIRST GO make writeHeader
+//IT GENERATES generatedInst.h -Wall -Wextra fail i do not now why
+//Then buildExe or buildExeSdl
+#define BVM_IMPLEMENTATION
+#include "bvm.h"
+#include "generatedInst.h"
+
+int main(){
+    Bvm vm = initBVM();
+    memcpy(vm.instruction, inst, sizeof(inst));
+    loop(&vm);
+    return 0;
+}
